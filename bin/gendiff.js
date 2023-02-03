7aaca00be370e5dd9d47df59769d604d5411fccf
olya kharlova <pepedrizzle@gmail.com>
2023-02-03 18:11:50 +0300

    gendiff.js modified with comparison action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import genDiff from '../src/index.js';
 import { Command } from 'commander';
 const program = new Command();
 
@@ -9,6 +10,6 @@ program
   .version('0.0.1')
   .option('-h, --help', 'display help for command')
   .option('-f, --format <type>', 'output format')
-  .action(() => program.outputHelp());
+  .action((filepath1, filepath2) => genDiff(filepath1, filepath2, program.opts().format));
 
 program.parse();
