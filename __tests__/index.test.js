de9ef237fbf807b39ce2254c4e722dd13aa65caf
olya kharlova <pepedrizzle@gmail.com>
2023-02-08 11:09:00 +0300

    index.test.js modified

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import genDiff from '../src/index.js';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
