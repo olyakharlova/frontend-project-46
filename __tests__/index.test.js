41b86173a0eb45fcaa898d65ce84056e23201b66
olya kharlova <pepedrizzle@gmail.com>
2023-02-08 12:51:16 +0300

    added tests for yml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,9 +9,15 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const firstJSON = getFixturePath('file1.json');
 const secondJSON = getFixturePath('file2.json');
+const firstYml = getFixturePath('file1.yml');
+const secondYml = getFixturePath('file2.yml');
 
 const resStr = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
 
 test('json files comparison', () => {
   expect(genDiff(firstJSON, secondJSON)).toEqual(resStr);
+});
+
+test('yml files comparison', () => {
+  expect(genDiff(firstYml, secondYml)).toEqual(resStr);
 });
