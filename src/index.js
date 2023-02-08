c80a6d10a88439795695ab9d0da6e7d913bf622b
olya kharlova <pepedrizzle@gmail.com>
2023-02-08 12:52:27 +0300

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { readFile } from './reading.js';
+import { readFile, getExtension } from './reading.js';
 import compareObjects from './compare.js';
+import parse from './parsers.js';
 import format from './formatting.js';
 
 const genDiff = (filepath1, filepath2) => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
 
-  const object1 = JSON.parse(file1);
-  const object2 = JSON.parse(file2);
+  const object1 = parse(file1, getExtension(filepath1));
+  const object2 = parse(file2, getExtension(filepath2));
 
   const res = compareObjects(object1, object2);
   const str = format(res);
