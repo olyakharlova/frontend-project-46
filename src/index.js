cf154329616d6c7f340b7737898d2d702dd0325a
olya kharlova <pepedrizzle@gmail.com>
2023-02-10 16:20:22 +0300

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { readFile, getExtension } from './reading.js';
-import compareObjects from './compare.js';
+import build from './builder.js';
 import parse from './parsers.js';
-import format from './formatting.js';
+import stylish from './formatters/stylish.js';
 
 const genDiff = (filepath1, filepath2) => {
   const file1 = readFile(filepath1);
@@ -10,8 +10,8 @@ const genDiff = (filepath1, filepath2) => {
   const object1 = parse(file1, getExtension(filepath1));
   const object2 = parse(file2, getExtension(filepath2));
 
-  const res = compareObjects(object1, object2);
-  const str = format(res);
+  const res = build(object1, object2);
+  const str = stylish(res);
 
   return str;
 };
