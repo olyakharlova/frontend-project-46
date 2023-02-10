cbfdadb8273849cddc69a7467fd161d3863b4570
olya kharlova <pepedrizzle@gmail.com>
2023-02-10 16:19:21 +0300

    tests added to index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,23 +1,23 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import genDiff from '../src/index.js';
+import { readFile } from '../src/reading.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const firstJSON = getFixturePath('file1.json');
-const secondJSON = getFixturePath('file2.json');
-const firstYml = getFixturePath('file1.yml');
-const secondYml = getFixturePath('file2.yml');
-
-const resStr = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true';
-
 test('json files comparison', () => {
-  expect(genDiff(firstJSON, secondJSON)).toEqual(resStr);
+  const res = readFile(getFixturePath('stylish.txt'));
+  const nestedJSON1 = getFixturePath('nestedFile1.json');
+  const nestedJSON2 = getFixturePath('nestedFile2.json');
+  expect(genDiff(nestedJSON1, nestedJSON2)).toEqual(res);
 });
 
 test('yml files comparison', () => {
-  expect(genDiff(firstYml, secondYml)).toEqual(resStr);
+  const res = readFile(getFixturePath('stylish.txt'));
+  const nestedYML1 = getFixturePath('nestedFile1.yaml');
+  const nestedYML2 = getFixturePath('nestedFile2.yaml');
+  expect(genDiff(nestedYML1, nestedYML2)).toBe(res);
 });
