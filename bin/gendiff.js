108e345f42d4d687aa0e3813a0140041d59c1278
olya kharlova <pepedrizzle@gmail.com>
2023-02-10 16:19:38 +0300

    gendiff.js modified

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-h, --help', 'display help for command')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, formatter) => {
+    console.log(genDiff(filepath1, filepath2, formatter.format));
   });
 
 program.parse();
