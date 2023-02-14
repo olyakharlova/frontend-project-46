e0b638eed7832a3352986e4f2e4ead8a76e69d30
olya kharlova <pepedrizzle@gmail.com>
2023-02-14 14:40:12 +0300

    fixed

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,7 +3,9 @@ import _ from 'lodash';
 const indent = (depth, times = 4) => ' '.repeat(depth * times);
 
 const stringify = (obj, depth) => {
-  if (!_.isPlainObject(obj)) return String(obj);
+  if (!_.isPlainObject(obj)) {
+    return String(obj);
+  }
   const lines = Object.entries(obj)
     .map(([key, value]) => `${indent(depth)}${key}: ${stringify(value, depth + 1)}`);
 
@@ -12,7 +14,7 @@ const stringify = (obj, depth) => {
 
 const stylish = (tree) => {
   const format = (obj, depth) => {
-    const lines = obj.flatMap((node) => {
+    const lines = obj.map((node) => {
       switch (node.type) {
         case 'added':
           return `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth + 1)}`;
@@ -25,13 +27,13 @@ const stylish = (tree) => {
         case 'updated':
           return [
             `${indent(depth - 1)}  - ${node.key}: ${stringify(node.value1, depth + 1)}`,
-            `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth - 1)}`,
+            `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth + 1)}`,
           ];
         default:
           return null;
       }
     });
-    return ['{', ...lines, `${indent(depth - 1)}}`].join('\n');
+    return ['{', ..._.flatten(lines), `${indent(depth - 1)}}`].join('\n');
   };
   return format(tree, 1);
 };
