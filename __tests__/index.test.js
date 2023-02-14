ce587c03d34b37b5e5a5b9e57d9144754eaf17de
olya kharlova <pepedrizzle@gmail.com>
2023-02-14 12:31:24 +0300

    index.test.js modified

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,16 +8,20 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('json files comparison', () => {
+test('nested files comparison, stylish format', () => {
   const res = readFile(getFixturePath('stylish.txt'));
   const nestedJSON1 = getFixturePath('nestedFile1.json');
   const nestedJSON2 = getFixturePath('nestedFile2.json');
-  expect(genDiff(nestedJSON1, nestedJSON2)).toEqual(res);
-});
-
-test('yml files comparison', () => {
-  const res = readFile(getFixturePath('stylish.txt'));
   const nestedYML1 = getFixturePath('nestedFile1.yaml');
   const nestedYML2 = getFixturePath('nestedFile2.yaml');
-  expect(genDiff(nestedYML1, nestedYML2)).toBe(res);
+  expect(genDiff(nestedJSON1, nestedJSON2, 'stylish')).toEqual(res);
+  expect(genDiff(nestedYML1, nestedYML2, 'stylish')).toEqual(res);
+  expect(genDiff(nestedJSON1, nestedYML2, 'stylish')).toEqual(res);
+});
+
+test('nested files, plain format', () => {
+  const res = readFile(getFixturePath('plain.txt'));
+  const nestedJSON1 = getFixturePath('nestedFile1.json');
+  const nestedJSON2 = getFixturePath('nestedFile2.json');
+  expect(genDiff(nestedJSON1, nestedJSON2, 'plain')).toEqual(res);
 });
