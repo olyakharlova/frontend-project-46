8fbd19350ea5e1fe274a67ad25bac9c54084db03
olya kharlova <pepedrizzle@gmail.com>
2023-02-14 12:31:45 +0300

    gendiff.js modified

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,11 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .arguments('<filepath1> <filepath2>')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-h, --help', 'display help for command')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'display help for command')
+  .option('-f, --format [plain, stylish]', 'output format', 'stylish')
   .action((filepath1, filepath2, formatter) => {
     console.log(genDiff(filepath1, filepath2, formatter.format));
   });
