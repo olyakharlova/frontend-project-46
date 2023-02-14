e5beaa078f1cc6dc403e482dbac37febc2fd88fd
olya kharlova <pepedrizzle@gmail.com>
2023-02-14 12:32:50 +0300

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import { readFile, getExtension } from './reading.js';
 import build from './builder.js';
 import parse from './parsers.js';
-import stylish from './formatters/stylish.js';
+import format from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
 
   const object1 = parse(file1, getExtension(filepath1));
   const object2 = parse(file2, getExtension(filepath2));
-
   const res = build(object1, object2);
-  const str = stylish(res);
 
-  return str;
+  return format(res, formatName);
 };
 
 export default genDiff;
